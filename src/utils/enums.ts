5bdcaa4eb23b3903da59c1a2671d7ff98354b07e
Ayse Gul Eren <ayse.eren@windowslive.com>
2023-05-04 18:45:01 +0300

    SideBar component creation

--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -4,3 +4,19 @@ export enum TOKEN_TYPES {
   NEW_NFT = 'New Nft',
   NFT_FROM_WALLET = 'Nft from Wallet',
 }
+
+export enum PAGES_NAME {
+  DAO = 'DAO',
+  VIEW_DAOS = 'View DAOs',
+  CREATE_DAO = 'Create DAO',
+  TOKEN = 'TOKEN',
+  MY_TOKENS = 'My Tokens',
+  GENERATE_TOKEN = 'Generate Token',
+  NFT = 'NFT',
+  MY_NFTS = 'My NFTs',
+  GENERATE_COLLECTION = 'Generate Collection',
+  GENERATE_NFT = 'Generate NFT',
+  DOCS = 'Docs',
+  DOCUMENTATION = 'Documentation',
+  LITEPAPER = 'Litepaper',
+}
