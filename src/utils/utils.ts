ba44e3a6dc4c61201e85eedcde05f69b3983a584
Ayse Gul Eren <ayse.eren@windowslive.com>
2023-05-05 13:50:44 +0300

    utils: base64ToImage  func addition

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -15,3 +15,12 @@ export const toDecimalsBN = (
 
   return new BN(BigNumber(num).multipliedBy(ten.pow(decimals)).toFixed(0));
 };
+
+export const base64ToImage = (
+  base64: any,
+  callback: (img: HTMLImageElement) => void,
+) => {
+  const img = new Image();
+  img.onload = () => callback(img);
+  img.src = base64;
+};
