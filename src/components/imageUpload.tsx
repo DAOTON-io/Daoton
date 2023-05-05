ef51c27d296169d91d4b3015d343c0fe863dd813
Ayse Gul Eren <ayse.eren@windowslive.com>
2023-05-05 13:51:50 +0300

    imageUpload fix

--- src/components/imageUpload.tsx
+++ src/components/imageUpload.tsx
@@ -33,24 +33,33 @@ const useStyles = makeStyles(theme => ({
 }));
 
 type Props = {
-  onChange: (image: string) => void;
+  onChange: (image: any) => void;
   onClear: () => void;
 };
 
 export const ImageUpload: React.FC<Props> = ({onChange, onClear}) => {
-  const classes = useStyles();
   const [file, setFile] = useState<File>();
+  const classes = useStyles();
   const inputRef = useRef<HTMLInputElement>(null);
 
   const handleFileInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
       const file = event.target.files[0];
 
-      console.log(Buffer.from(JSON.stringify(file)).toString('base64'));
-      console.log(window.btoa(JSON.stringify(file)));
+      // console.log(Buffer.from(JSON.stringify(file)).toString('base64'));
+      // console.log(window.btoa(JSON.stringify(file)));
+
+      let fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+
+      fileReader.onload = event => {
+        if (event.target?.result) {
+          onChange(event.target.result);
+        }
+      };
 
       setFile(file);
-      onChange(Buffer.from(JSON.stringify(file)).toString('base64'));
+      //onChange(Buffer.from(JSON.stringify(file)).toString('base64'));
     }
   };
 
