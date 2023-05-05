0870af7682ebe68bbcc2514a44130ab10a21a15a
Ayse Gul Eren <ayse.eren@windowslive.com>
2023-05-05 14:59:53 +0300

    Review: image showing fix

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -1,8 +1,14 @@
-import React from "react";
-import { Grid, Stack, Theme, Typography } from "@mui/material";
-import { makeStyles } from "@mui/styles";
-import { CategoryType, InfoType, TokenDetailType, NftDetailType } from "../utils/types";
-import { CustomButton } from "./CustomButton";
+import React, {useEffect} from 'react';
+import {Grid, Stack, Theme, Typography} from '@mui/material';
+import {makeStyles} from '@mui/styles';
+import {
+  CategoryType,
+  InfoType,
+  TokenDetailType,
+  NftDetailType,
+} from '../utils/types';
+import {CustomButton} from './CustomButton';
+import {base64ToImage} from '../utils/utils';
 
 type Props = {
   selectedCategory: CategoryType;
@@ -16,27 +22,73 @@ const useStyles = makeStyles((theme: Theme) => ({
   container: {
     marginBottom: 6,
     marginTop: 6,
-    [theme.breakpoints.down("sm")]: {
+    [theme.breakpoints.down('sm')]: {
       marginBottom: 2,
       marginTop: 2,
-      padding: "24px",
+      padding: '24px',
     },
-    display: "flex",
-    justifyContent: "center",
-    width: "100% !important",
+    display: 'flex',
+    justifyContent: 'center',
+    width: '100% !important',
     padding: theme.spacing(2),
     borderRadius: theme.spacing(1),
+    overflow: 'auto',
+  },
+  buttonContainer: {
+    paddingRight: '2rem',
+    paddingLeft: '2rem',
+    textAlign: 'start',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: '0.5rem',
+    [theme.breakpoints.down('sm')]: {
+      paddingRight: '1rem',
+      paddingLeft: '1rem',
+    },
   },
 }));
 
-export const Review: React.FC<Props> = ({ selectedCategory, daoInfo, tokenDetail, nftDetail, activeStepOnChange }) => {
+export const Review: React.FC<Props> = ({
+  selectedCategory,
+  daoInfo,
+  tokenDetail,
+  nftDetail,
+  activeStepOnChange,
+}) => {
   const classes = useStyles();
 
+  useEffect(() => {
+    base64ToImage(daoInfo.image, img => {
+      document.getElementById('dao-image')!.style.width = '200px';
+      document.getElementById('dao-image')!.style.height = '200px';
+      (document.getElementById('dao-image') as HTMLInputElement)!.src =
+        daoInfo.image;
+    });
+  }, [daoInfo.image]);
+
+  useEffect(() => {
+    base64ToImage(tokenDetail.image, img => {
+      document.getElementById('token-image')!.style.width = '200px';
+      document.getElementById('token-image')!.style.height = '200px';
+      (document.getElementById('token-image') as HTMLInputElement)!.src =
+        tokenDetail.image;
+    });
+  }, [tokenDetail.image]);
+
+  useEffect(() => {
+    base64ToImage(nftDetail.image, img => {
+      document.getElementById('nft-image')!.style.width = '200px';
+      document.getElementById('nft-image')!.style.height = '200px';
+      (document.getElementById('nft-image') as HTMLInputElement)!.src =
+        nftDetail.image;
+    });
+  }, [nftDetail.image]);
+
   const save = () => {
-    console.log("category in review", selectedCategory);
-    console.log("daoInfo in review", daoInfo);
-    console.log("tokenDetail in review", tokenDetail);
-    console.log("nftDetail in review", nftDetail);
+    console.log('category in review', selectedCategory);
+    console.log('daoInfo in review', daoInfo);
+    console.log('tokenDetail in review', tokenDetail);
+    console.log('nftDetail in review', nftDetail);
   };
 
   const backStep = () => {
@@ -51,19 +103,17 @@ export const Review: React.FC<Props> = ({ selectedCategory, daoInfo, tokenDetail
             direction="row"
             spacing={2}
             style={{
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
+              display: 'flex',
+              justifyContent: 'center',
+            }}>
             <Stack direction="column" spacing={2}>
               <Grid
                 style={{
-                  padding: "1rem",
-                  backgroundColor: "#2C6495",
-                  borderRadius: "1rem",
-                  color: "beige",
-                }}
-              >
+                  padding: '1rem',
+                  backgroundColor: '#2C6495',
+                  borderRadius: '1rem',
+                  color: 'beige',
+                }}>
                 <Typography variant="body1">
                   <div>
                     <b>Category: </b>
@@ -82,6 +132,14 @@ export const Review: React.FC<Props> = ({ selectedCategory, daoInfo, tokenDetail
                     {daoInfo.desc}
                   </div>
                 </Typography>
+                <Grid container className={classes.buttonContainer}>
+                  <Grid item justifyContent={'flex-start'}>
+                    <label>DAO Image : </label>
+                  </Grid>
+                  <Grid item justifyContent={'flex-end'}>
+                    <img id="dao-image" alt="alt"></img>
+                  </Grid>
+                </Grid>
               </Grid>
             </Stack>
             {tokenDetail ? (
@@ -89,12 +147,11 @@ export const Review: React.FC<Props> = ({ selectedCategory, daoInfo, tokenDetail
                 <Grid
                   item
                   style={{
-                    padding: "1rem",
-                    backgroundColor: "#2C6495",
-                    borderRadius: "1rem",
-                    color: "beige",
-                  }}
-                >
+                    padding: '1rem',
+                    backgroundColor: '#2C6495',
+                    borderRadius: '1rem',
+                    color: 'beige',
+                  }}>
                   <Typography variant="body1">
                     <div>
                       <b>Token Name: </b>
@@ -138,28 +195,57 @@ export const Review: React.FC<Props> = ({ selectedCategory, daoInfo, tokenDetail
                     </div>
                   </Typography>
                 </Grid>
+                {tokenDetail.image && (
+                  <Grid container className={classes.buttonContainer}>
+                    <Grid item justifyContent={'flex-start'}>
+                      <label>Token Image : </label>
+                    </Grid>
+                    <Grid item justifyContent={'flex-end'}>
+                      <div id="token-image"></div>
+                    </Grid>
+                  </Grid>
+                )}
               </Stack>
             ) : nftDetail ? (
               <Stack direction="column" spacing={2}>
                 <Grid
                   item
                   style={{
-                    padding: "1rem",
-                    backgroundColor: "#2C6495",
-                    borderRadius: "1rem",
-                    color: "beige",
-                  }}
-                >
-                  <Typography variant="body1">Nft Name: {nftDetail.name}</Typography>
-                  <Typography variant="body1">Token Description: {nftDetail.description}</Typography>
-                  <Typography variant="body1">Nft Level: {nftDetail.level}</Typography>
-                  <Typography variant="body1">Collection Address: {nftDetail.collectionAddress}</Typography>
+                    padding: '1rem',
+                    backgroundColor: '#2C6495',
+                    borderRadius: '1rem',
+                    color: 'beige',
+                  }}>
+                  <Typography variant="body1">
+                    Nft Name: {nftDetail.name}
+                  </Typography>
+                  <Typography variant="body1">
+                    Token Description: {nftDetail.description}
+                  </Typography>
+                  <Typography variant="body1">
+                    Nft Level: {nftDetail.level}
+                  </Typography>
+                  <Typography variant="body1">
+                    Collection Address: {nftDetail.collectionAddress}
+                  </Typography>
+                </Grid>
+                <Grid container className={classes.buttonContainer}>
+                  <Grid item justifyContent={'flex-start'}>
+                    <label>Nft Image : </label>
+                  </Grid>
+                  <Grid item justifyContent={'flex-end'}>
+                    <div id="nft-image"></div>
+                  </Grid>
                 </Grid>
               </Stack>
             ) : undefined}
           </Stack>
         </Grid>
-        <Grid paddingTop={2} container justifyContent={"space-between"} width={"100%"}>
+        <Grid
+          paddingTop={2}
+          container
+          justifyContent={'space-between'}
+          width={'100%'}>
           <CustomButton onClick={backStep} disabled={false} label="BACK" />
           <CustomButton onClick={save} disabled={false} label="SAVE" />
         </Grid>
