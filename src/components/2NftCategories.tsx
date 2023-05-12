8cf7a757a87ba1063586faad3752d5bf5cdd26bf
Ayse Gul Eren <ayse.eren@windowslive.com>
2023-05-12 15:56:39 +0300

    NftCategories: css

--- src/components/2NftCategories.tsx
+++ src/components/2NftCategories.tsx
@@ -1,21 +1,55 @@
-import React from "react";
+import React from 'react';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
-import { makeStyles } from "@mui/styles";
-import { Box, Card, CardActionArea, CardContent, Grid, Theme, Typography } from "@mui/material";
-import { CategoryType } from "../utils/types";
+import {makeStyles} from '@mui/styles';
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  Grid,
+  Theme,
+  Typography,
+} from '@mui/material';
+import {CategoryType} from '../utils/types';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  card: {
+    backgroundImage: "url('/images/bluebg2.jpg') !important",
+    backgroundSize: 'cover',
+  },
+  cardSelected: {
+    backgroundColor: '#EC7D31 !important',
+    backgroundSize: 'cover',
+  },
+  cardItem: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '5rem',
+  },
+}));
 
 const categories: CategoryType[] = [
   {
     id: 1,
-    label: "Generate your NFT",
-
-    icon: <DashboardIcon style={{ color: "white", marginRight: "1rem" }} fontSize="large" />,
+    label: 'Generate Your NFT',
+    icon: (
+      <DashboardIcon
+        style={{color: 'white', marginRight: '1rem'}}
+        fontSize="large"
+      />
+    ),
   },
   {
     id: 2,
-    label: "Generate your Collection",
-    icon: <LibraryBooksIcon style={{ color: "white", marginRight: "1rem" }} fontSize="large" />,
+    label: 'Generate Your Collection',
+    icon: (
+      <LibraryBooksIcon
+        style={{color: 'white', marginRight: '1rem'}}
+        fontSize="large"
+      />
+    ),
   },
 ];
 
@@ -25,30 +59,17 @@ type Props = {
   selectedCategory: CategoryType;
 };
 
-const useStyles = makeStyles((theme: Theme) => ({
-  card: {
-    backgroundImage: "url('/images/bluebg2.jpg') !important",
-    backgroundSize: "cover",
-  },
-  cardSelected: {
-    backgroundColor: "#EC7D31 !important",
-    backgroundSize: "cover",
-  },
-  cardItem: {
-    display: "flex",
-    alignItem: "center",
-    justifyContent: "center",
-    height: "5rem",
-  },
-}));
-
-export const NFTCategories: React.FC<Props> = ({ activeStepOnChange, selectedCategoryOnChange, selectedCategory }) => {
+export const NFTCategories: React.FC<Props> = ({
+  activeStepOnChange,
+  selectedCategoryOnChange,
+  selectedCategory,
+}) => {
   const classes = useStyles();
 
   return (
-    <div style={{width:'100%'}}>
+    <div style={{width: '100%'}}>
       <Grid container spacing={2}>
-        {categories.map((category) => {
+        {categories.map(category => {
           if (category.id === selectedCategory.id) {
             return (
               <Grid item xs={12} sm={6} key={category.id.toString()}>
@@ -59,12 +80,18 @@ export const NFTCategories: React.FC<Props> = ({ activeStepOnChange, selectedCat
                     activeStepOnChange(2);
                     selectedCategoryOnChange(category);
                   }}
-                  key={category.id.toString()}
-                >
+                  key={category.id.toString()}>
                   <CardActionArea key={category.id.toString()}>
-                    <CardContent className={classes.cardItem} key={category.id.toString()}>
+                    <CardContent
+                      className={classes.cardItem}
+                      key={category.id.toString()}>
                       {category.icon}
-                      <Typography color={"white"} fontSize={30} gutterBottom variant="h5" component="div">
+                      <Typography
+                        color={'white'}
+                        fontSize={30}
+                        gutterBottom
+                        variant="h5"
+                        component="div">
                         {category.label}
                       </Typography>
                     </CardContent>
@@ -83,12 +110,18 @@ export const NFTCategories: React.FC<Props> = ({ activeStepOnChange, selectedCat
                     activeStepOnChange(2);
                     selectedCategoryOnChange(category);
                   }}
-                  key={category.id.toString()}
-                >
+                  key={category.id.toString()}>
                   <CardActionArea key={category.id.toString()}>
-                    <CardContent className={classes.cardItem} key={category.id.toString()}>
+                    <CardContent
+                      className={classes.cardItem}
+                      key={category.id.toString()}>
                       {category.icon}
-                      <Typography color={"white"} fontSize={30} gutterBottom variant="h5" component="div">
+                      <Typography
+                        color={'white'}
+                        fontSize={24}
+                        gutterBottom
+                        variant="h5"
+                        component="div">
                         {category.label}
                       </Typography>
                     </CardContent>
