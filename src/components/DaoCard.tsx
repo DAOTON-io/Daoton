d9aabb13ac52719d509bdb8b87047e9e434cfe42
afarukcali <afarukcali@gmail.com>
2023-05-15 17:29:23 +0300

    View dao redirect.

--- src/components/DaoCard.tsx
+++ src/components/DaoCard.tsx
@@ -1,60 +1,62 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React from 'react';
-import {Grid, IconButton} from '@mui/material';
-import {makeStyles} from '@mui/styles';
-import GoogleFontLoader from 'react-google-font-loader';
-import {Card} from 'reactstrap';
-import {CopyAll} from '@mui/icons-material';
+import React from "react";
+import { Grid, IconButton } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import GoogleFontLoader from "react-google-font-loader";
+import { Card } from "reactstrap";
+import { CopyAll } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 
 const useStyles = makeStyles({
   container: {
-    padding: '1rem',
+    padding: "1rem",
   },
   card: {
-    backgroundColor: 'white',
-    boxShadow: '0 0 10px 0 rgba(0,0,0,0.1)',
-    borderRadius: '0.5rem',
-    padding: '20px',
-    minHeight: '40vh',
+    backgroundColor: "white",
+    boxShadow: "0 0 10px 0 rgba(0,0,0,0.1)",
+    borderRadius: "0.5rem",
+    padding: "20px",
+    minHeight: "40vh",
+    cursor: "pointer",
   },
   name: {
-    color: 'white',
-    justifyContent: 'center !important ',
-    alignItems: 'center !important',
-    display: 'flex',
-    width: '100%',
-    margin: '10px',
-    fontFamily: 'Signika Negative',
-    backgroundColor: '#ff761c',
-    borderRadius: '0.5rem',
-    padding: '5px',
+    color: "white",
+    justifyContent: "center !important ",
+    alignItems: "center !important",
+    display: "flex",
+    width: "100%",
+    margin: "10px",
+    fontFamily: "Signika Negative",
+    backgroundColor: "#ff761c",
+    borderRadius: "0.5rem",
+    padding: "5px",
   },
   date: {
-    color: 'black',
-    justifyContent: 'center',
-    alignItems: 'center',
-    display: 'flex',
-    fontSize: '12px',
-    marginBottom: '10px',
-    fontFamily: 'Signika Negative',
+    color: "black",
+    justifyContent: "center",
+    alignItems: "center",
+    display: "flex",
+    fontSize: "12px",
+    marginBottom: "10px",
+    fontFamily: "Signika Negative",
   },
   value: {
-    align: 'center !important',
-    color: 'black',
-    justifyContent: 'center !important',
-    alignItems: 'center !important',
-    display: 'flex',
-    fontSize: '14px',
-    fontFamily: 'Signika Negative',
-    marginBottom: '10px',
+    align: "center !important",
+    color: "black",
+    justifyContent: "center !important",
+    alignItems: "center !important",
+    display: "flex",
+    fontSize: "14px",
+    fontFamily: "Signika Negative",
+    marginBottom: "10px",
   },
   description: {
-    color: 'black',
-    justifyContent: 'center',
-    alignItems: 'center',
-    display: 'flex',
-    fontFamily: 'Signika Negative',
-    fontSize: '1rem',
+    color: "black",
+    justifyContent: "center",
+    alignItems: "center",
+    display: "flex",
+    fontFamily: "Signika Negative",
+    fontSize: "1rem",
   },
 });
 
@@ -65,100 +67,85 @@ type Props = {
   value: string;
   daoId: string;
   daoImg: string;
-  path?: string;
 };
 
-export const DaoCard: React.FC<Props> = ({
-  name,
-  date,
-  description,
-  value,
-  daoId,
-  daoImg,
-  path,
-}) => {
+export const DaoCard: React.FC<Props> = ({ name, date, description, value, daoId, daoImg }) => {
   const classes = useStyles();
+  const navigate = useNavigate();
 
-  console.log('daoId', daoId);
   return (
     <div>
       <GoogleFontLoader
         fonts={[
           {
-            font: 'Signika Negative',
-            weights: [400, '400i'],
+            font: "Signika Negative",
+            weights: [400, "400i"],
           },
         ]}
-        subsets={['cyrillic-ext', 'greek']}
+        subsets={["cyrillic-ext", "greek"]}
       />
       <div className={classes.container}>
-        <a
-          style={{
-            textDecoration: 'none',
+        <Card
+          className={classes.card}
+          onClick={() => {
+            navigate("/view-dao/" + daoId);
           }}
-          href={path + daoId}
-          target="_blank"
-          rel="noreferrer">
-          <Card className={classes.card}>
-            <div
-              style={{
-                justifyContent: 'center',
-                alignItems: 'center',
-                display: 'flex',
-              }}>
-              <Grid item>
-                <div>
-                  <div
-                    style={{
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                      display: 'flex',
-                      height: '6rem',
-                    }}>
-                    <img
-                      style={{objectFit: 'contain', padding: '1rem'}}
-                      width={200}
-                      height={200}
-                      src={daoImg || 'images/logobg.png'}
-                    />
+        >
+          <div
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              display: "flex",
+            }}
+          >
+            <Grid item>
+              <div>
+                <div
+                  style={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                    display: "flex",
+                    height: "6rem",
+                  }}
+                >
+                  <img style={{ objectFit: "contain", padding: "1rem" }} width={200} height={200} src={daoImg || "images/logobg.png"} />
+                </div>
+              </div>
+              <div style={{ marginTop: "1rem" }}>
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                  <Grid container width={"13rem"}>
+                    <p className={classes.name}>{name}</p>
+                  </Grid>
+                </div>
+                <p className={classes.date}>Created on {date}</p>
+                <p className={classes.description}>{description}</p>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "end",
+                  }}
+                >
+                  <div style={{ width: "80%" }}>
+                    <p className={classes.value}>{value.slice(0, 13) + "..." + value.slice(-3)}</p>
+                  </div>
+                  <div style={{ width: "20%" }}>
+                    <IconButton
+                      color="primary"
+                      aria-label="upload picture"
+                      component="label"
+                      onClick={() => {
+                        navigator.clipboard.writeText(value);
+                      }}
+                    >
+                      <CopyAll />
+                    </IconButton>
                   </div>
                 </div>
-                <div style={{marginTop: '1rem'}}>
-                  <div style={{display: 'flex', justifyContent: 'center'}}>
-                    <Grid container width={'13rem'}>
-                      <p className={classes.name}>{name}</p>
-                    </Grid>
-                  </div>
-                  <p className={classes.date}>Created on {date}</p>
-                  <p className={classes.description}>{description}</p>
-                  <div
-                    style={{
-                      display: 'flex',
-                      justifyContent: 'center',
-                      alignItems: 'end',
-                    }}>
-                    <div style={{width: '80%'}}>
-                      <p className={classes.value}>
-                        {value.slice(0, 13) + '...' + value.slice(-3)}
-                      </p>
-                    </div>
-                    <div style={{width: '20%'}}>
-                      <IconButton
-                        color="primary"
-                        aria-label="upload picture"
-                        component="label"
-                        onClick={() => {
-                          navigator.clipboard.writeText(value);
-                        }}>
-                        <CopyAll />
-                      </IconButton>
-                    </div>
-                  </div>
-                </div>
-              </Grid>
-            </div>
-          </Card>
-        </a>
+              </div>
+            </Grid>
+          </div>
+        </Card>
       </div>
     </div>
   );
--- src/pages/DaoDetail.tsx
+++ src/pages/DaoDetail.tsx
@@ -1,73 +1,59 @@
-import React, {useEffect, useState} from 'react';
-import {Address} from 'ton-core';
-import {useNavigate, useParams} from 'react-router-dom';
-import {TonClient} from 'ton';
-import {getHttpEndpoint} from '@orbs-network/ton-access';
-import DaoContract from '../lib/dao/lib/DaoContract';
-import {open} from '../utils/index';
-import {
-  Card,
-  CardContent,
-  CircularProgress,
-  Grid,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Theme,
-  Typography,
-} from '@mui/material';
-import {makeStyles} from '@mui/styles';
-import {CustomButton} from '../components/CustomButton';
-import {Dao} from '../utils/types';
-import {categories} from '../components/DaoCategories';
+import React, { useEffect, useState } from "react";
+import { Address } from "ton-core";
+import { useNavigate, useParams } from "react-router-dom";
+import { TonClient } from "ton";
+import { getHttpEndpoint } from "@orbs-network/ton-access";
+import DaoContract from "../lib/dao/lib/DaoContract";
+import { open } from "../utils/index";
+import { Card, CardContent, CircularProgress, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Theme, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { CustomButton } from "../components/CustomButton";
+import { Dao } from "../utils/types";
+import { categories } from "../components/DaoCategories";
 
 const useStyles = makeStyles((theme: Theme) => ({
   cardContainer: {
-    display: 'flex',
-    justifyContent: 'center',
-    margin: '1rem !important',
-    [theme.breakpoints.down('sm')]: {
+    display: "flex",
+    justifyContent: "center",
+    margin: "1rem !important",
+    [theme.breakpoints.down("sm")]: {
       marginBottom: 2,
       marginTop: 2,
-      padding: '24px',
+      padding: "24px",
     },
   },
   tableContainer: {
-    display: 'flex',
-    justifyContent: 'center',
-    padding: '1rem !important',
-    [theme.breakpoints.down('sm')]: {
+    display: "flex",
+    justifyContent: "center",
+    padding: "1rem !important",
+    [theme.breakpoints.down("sm")]: {
       marginBottom: 2,
       marginTop: 2,
-      padding: '24px',
+      padding: "24px",
     },
   },
   item: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: '0 !important',
-    margin: '0 !important',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "0 !important",
+    margin: "0 !important",
   },
   card: {
-    width: '100%',
-    marginBottom: '0.8rem',
-    border: '1px solid #2C6495',
+    width: "100%",
+    marginBottom: "0.8rem",
+    border: "1px solid #2C6495",
   },
   buttonItem: {
-    display: 'flex',
-    width: '100%',
-    justifyContent: 'flex-end',
-    marginBottom: '1rem !important',
+    display: "flex",
+    width: "100%",
+    justifyContent: "flex-end",
+    marginBottom: "1rem !important",
   },
   tableItem: {
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'flex-end',
+    width: "100%",
+    display: "flex",
+    justifyContent: "flex-end",
   },
 }));
 
@@ -75,7 +61,7 @@ const DaoDetail: React.FC = () => {
   const [daoValues, setDaoValues] = useState<Dao>();
   const [loading, setLoading] = useState<boolean>(true);
 
-  const {daoId} = useParams();
+  const { daoId } = useParams();
   const classes = useStyles();
   const navigate = useNavigate();
 
@@ -85,8 +71,8 @@ const DaoDetail: React.FC = () => {
         const daoContractAddress = Address.parse(daoId);
         const daoMasterContract = new DaoContract(daoContractAddress);
 
-        const endpoint = await getHttpEndpoint({network: 'testnet'});
-        const client = new TonClient({endpoint});
+        const endpoint = await getHttpEndpoint({ network: "testnet" });
+        const client = new TonClient({ endpoint });
         const daoContract = open(daoMasterContract, client);
         const daoData = await daoContract.getDaoData();
 
@@ -102,12 +88,13 @@ const DaoDetail: React.FC = () => {
     return (
       <div
         style={{
-          height: 'calc(100vh - 8rem)',
-          width: '100%',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
+          height: "calc(100vh - 8rem)",
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <CircularProgress />
       </div>
     );
@@ -115,47 +102,34 @@ const DaoDetail: React.FC = () => {
     return (
       <div
         style={{
-          height: 'calc(100vh - 8.5rem)',
-          width: '100%',
-          overflow: 'auto',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}>
+          height: "calc(100vh - 8.5rem)",
+          width: "100%",
+          overflow: "auto",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
         <Grid container className={classes.cardContainer}>
-          <Grid
-            item
-            justifyContent={'center'}
-            className={classes.item}
-            sx={{flexDirection: 'column !important'}}>
+          <Grid item justifyContent={"center"} className={classes.item} sx={{ flexDirection: "column !important" }}>
             <Card className={classes.card}>
               <CardContent>
-                <Typography sx={{mb: 1.5}} color="#2C6495">
+                <Typography sx={{ mb: 1.5 }} color="#2C6495">
                   <b>DAO Name:</b> {daoValues?.content.name}
                 </Typography>
-                <Typography sx={{mb: 1.5}} color="#2C6495">
+                <Typography sx={{ mb: 1.5 }} color="#2C6495">
                   <b>DAO Address:</b> {daoValues?.address}
                 </Typography>
-                <Typography sx={{mb: 1.5}} color="#2C6495">
-                  <b>DAO Token Address:</b>{' '}
-                  {daoValues?.tokenContract.toFriendly()}
+                <Typography sx={{ mb: 1.5 }} color="#2C6495">
+                  <b>DAO Token Address:</b> {daoValues?.tokenContract.toFriendly()}
                 </Typography>
                 {daoValues?.nftContract ? (
-                  <Typography sx={{mb: 1.5}} color="#2C6495">
-                    <b>DAO NFT Address:</b>{' '}
-                    {daoValues?.nftContract.toFriendly()}
+                  <Typography sx={{ mb: 1.5 }} color="#2C6495">
+                    <b>DAO NFT Address:</b> {daoValues?.nftContract.toFriendly()}
                   </Typography>
                 ) : undefined}
-                <Typography sx={{mb: 1.5}} color="#2C6495">
-                  It is a{' '}
-                  <b>
-                    {
-                      categories.find(
-                        category => category.id === daoValues?.daoTypeId,
-                      )?.label
-                    }
-                  </b>{' '}
-                  category. {daoValues?.content.description}
+                <Typography sx={{ mb: 1.5 }} color="#2C6495">
+                  It is a <b>{categories.find((category) => category.id === daoValues?.daoTypeId)?.label}</b> category. {daoValues?.content.description}
                 </Typography>
               </CardContent>
             </Card>
@@ -163,20 +137,11 @@ const DaoDetail: React.FC = () => {
         </Grid>
         <Grid container className={classes.tableContainer}>
           <Grid item className={classes.buttonItem}>
-            <CustomButton
-              label="Create Proposal"
-              onClick={() =>
-                navigate(`/view-dao/${daoValues?.address}/create-proposal`)
-              }
-            />
+            <CustomButton label="Create Proposal" onClick={() => navigate(`/view-dao/${daoValues?.address}/create-proposal`)} />
           </Grid>
-          <Grid
-            item
-            justifyContent={'center'}
-            className={classes.tableItem}
-            sx={{flexDirection: 'column !important'}}>
+          <Grid item justifyContent={"center"} className={classes.tableItem} sx={{ flexDirection: "column !important" }}>
             <TableContainer component={Paper}>
-              <Table sx={{minWidth: 650}} aria-label="simple table">
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
                   <TableRow>
                     <TableCell>Dessert (100g serving)</TableCell>
