698964185fda6d10bf561df040799b4666563ab0
Ayse Gul Eren <ayse.eren@windowslive.com>
2023-05-16 14:55:38 +0300

    DaoDetail: css

--- src/pages/DaoDetail.tsx
+++ src/pages/DaoDetail.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import {Address} from 'ton-core';
 import {useNavigate, useParams} from 'react-router-dom';
 import {TonClient} from 'ton';
@@ -16,6 +16,7 @@ import {
   TableCell,
   TableContainer,
   TableHead,
+  TablePagination,
   TableRow,
   Theme,
   Typography,
@@ -27,7 +28,6 @@ import {categories} from '../components/DaoCategories';
 
 const useStyles = makeStyles((theme: Theme) => ({
   cardContainer: {
-    display: 'flex',
     justifyContent: 'center',
     margin: '1rem !important',
     [theme.breakpoints.down('sm')]: {
@@ -37,9 +37,8 @@ const useStyles = makeStyles((theme: Theme) => ({
     },
   },
   tableContainer: {
-    display: 'flex',
     justifyContent: 'center',
-    padding: '1rem !important',
+    padding: '0 1rem 2rem 1rem;',
     [theme.breakpoints.down('sm')]: {
       marginBottom: 2,
       marginTop: 2,
@@ -69,6 +68,13 @@ const useStyles = makeStyles((theme: Theme) => ({
     display: 'flex',
     justifyContent: 'flex-end',
     overflowX: 'auto',
+    border: '1px solid #2C6495',
+    borderRadius: '4px',
+  },
+  table: {overflowX: 'auto'},
+  tableHeader: {
+    color: '#2C6495 !important',
+    fontWeight: 'bold !important',
   },
 }));
 
@@ -76,6 +82,8 @@ const DaoDetail: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(true);
   const [daoValues, setDaoValues] = useState<Dao>();
   const [proposals, setProposals] = useState<ProposalType[]>();
+  const [page, setPage] = useState<number>(0);
+  const [rowsPerPage, setRowsPerPage] = useState<number>(10);
 
   const classes = useStyles();
   const {daoId} = useParams();
@@ -98,14 +106,21 @@ const DaoDetail: React.FC = () => {
 
         setDaoValues(dao);
         setProposals(proposals);
-        console.log(proposals);
-
         setLoading(false);
       }
     };
 
     init();
   }, [daoId]);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
   if (loading) {
     return (
@@ -126,7 +141,8 @@ const DaoDetail: React.FC = () => {
         style={{
           height: 'calc(100vh - 8.5rem)',
           width: '100%',
-          overflow: 'auto',
+          overflowY: 'auto',
+          overflowX: 'hidden',
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
@@ -139,23 +155,23 @@ const DaoDetail: React.FC = () => {
             sx={{flexDirection: 'column !important'}}>
             <Card className={classes.card}>
               <CardContent>
-                <Typography sx={{mb: 1.5}} color="#2C6495">
+                <Typography sx={{mb: 0.5}} color="#2C6495">
                   <b>DAO Name:</b> {daoValues?.content.name}
                 </Typography>
-                <Typography sx={{mb: 1.5}} color="#2C6495">
+                <Typography sx={{mb: 0.5}} color="#2C6495">
                   <b>DAO Address:</b> {daoValues?.address}
                 </Typography>
-                <Typography sx={{mb: 1.5}} color="#2C6495">
+                <Typography sx={{mb: 0.5}} color="#2C6495">
                   <b>DAO Token Address:</b>{' '}
                   {daoValues?.tokenContract.toFriendly()}
                 </Typography>
                 {daoValues?.nftContract ? (
-                  <Typography sx={{mb: 1.5}} color="#2C6495">
+                  <Typography sx={{mb: 0.5}} color="#2C6495">
                     <b>DAO NFT Address:</b>{' '}
                     {daoValues?.nftContract.toFriendly()}
                   </Typography>
                 ) : undefined}
-                <Typography sx={{mb: 1.5}} color="#2C6495">
+                <Typography sx={{mb: 0.5}} color="#2C6495">
                   It is a{' '}
                   <b>
                     {
@@ -184,65 +200,200 @@ const DaoDetail: React.FC = () => {
             justifyContent={'center'}
             className={classes.tableItem}
             sx={{flexDirection: 'column !important'}}>
-            <TableContainer component={Paper} style={{overflowX: 'auto'}}>
-              <Table aria-label="simple table" style={{minWidth: '800px'}}>
+            <TableContainer component={Paper} className={classes.table}>
+              <Table
+                aria-label="proposal table"
+                stickyHeader
+                style={{minWidth: '800px'}}>
                 <TableHead>
                   <TableRow>
-                    <TableCell align="right">Content</TableCell>
-                    <TableCell align="right">Owner</TableCell>
-                    <TableCell align="right">Timestamp</TableCell>
-                    <TableCell align="right">Balance</TableCell>
-                    <TableCell align="right">Vote Count</TableCell>
-                    <TableCell align="right">Yes Count</TableCell>
-                    <TableCell align="right">No Count</TableCell>
-                    <TableCell align="right">Abstain Count</TableCell>
-                    <TableCell align="right">Success Threshold</TableCell>
-                    <TableCell align="right">Fail Threshold</TableCell>
-                    <TableCell align="right">Is Related With NFT?</TableCell>
+                    <TableCell
+                      align="right"
+                      style={{
+                        paddingRight: '8px',
+                        borderBottom: '1px solid #2C6495',
+                      }}>
+                      <Typography
+                        className={classes.tableHeader}
+                        variant="subtitle1">
+                        Content
+                      </Typography>
+                    </TableCell>
+                    <TableCell
+                      align="right"
+                      style={{
+                        paddingRight: '8px',
+                        borderBottom: '1px solid #2C6495',
+                      }}>
+                      <Typography
+                        className={classes.tableHeader}
+                        variant="subtitle1">
+                        Owner
+                      </Typography>
+                    </TableCell>
+                    <TableCell
+                      align="right"
+                      style={{
+                        paddingRight: '8px',
+                        borderBottom: '1px solid #2C6495',
+                      }}>
+                      <Typography
+                        className={classes.tableHeader}
+                        variant="subtitle1">
+                        Timestamp
+                      </Typography>
+                    </TableCell>
+                    <TableCell
+                      align="right"
+                      style={{
+                        paddingRight: '8px',
+                        borderBottom: '1px solid #2C6495',
+                      }}>
+                      <Typography
+                        className={classes.tableHeader}
+                        variant="subtitle1">
+                        Balance
+                      </Typography>
+                    </TableCell>
+                    <TableCell align="right" style={{paddingRight: '8px'}}>
+                      <Typography
+                        className={classes.tableHeader}
+                        variant="subtitle1">
+                        Vote Count
+                      </Typography>
+                    </TableCell>
+                    <TableCell
+                      align="right"
+                      style={{
+                        paddingRight: '8px',
+                        borderBottom: '1px solid #2C6495',
+                      }}>
+                      <Typography
+                        variant="subtitle1"
+                        className={classes.tableHeader}>
+                        Yes Count
+                      </Typography>
+                    </TableCell>
+                    <TableCell
+                      align="right"
+                      style={{
+                        paddingRight: '8px',
+                        borderBottom: '1px solid #2C6495',
+                      }}>
+                      <Typography
+                        className={classes.tableHeader}
+                        variant="subtitle1">
+                        No Count
+                      </Typography>
+                    </TableCell>
+                    <TableCell
+                      align="right"
+                      style={{
+                        paddingRight: '8px',
+                        borderBottom: '1px solid #2C6495',
+                      }}>
+                      <Typography
+                        className={classes.tableHeader}
+                        variant="subtitle1">
+                        Abstain Count
+                      </Typography>
+                    </TableCell>
+                    <TableCell
+                      align="right"
+                      style={{
+                        paddingRight: '8px',
+                        borderBottom: '1px solid #2C6495',
+                      }}>
+                      <Typography
+                        className={classes.tableHeader}
+                        variant="subtitle1">
+                        Success Threshold
+                      </Typography>
+                    </TableCell>
+                    <TableCell
+                      align="right"
+                      style={{
+                        paddingRight: '8px',
+                        borderBottom: '1px solid #2C6495',
+                      }}>
+                      <Typography
+                        className={classes.tableHeader}
+                        variant="subtitle1">
+                        Fail Threshold
+                      </Typography>
+                    </TableCell>
+                    <TableCell
+                      align="right"
+                      style={{
+                        paddingRight: '8px',
+                        borderBottom: '1px solid #2C6495',
+                      }}>
+                      <Typography
+                        className={classes.tableHeader}
+                        variant="subtitle1">
+                        Is Related With NFT?
+                      </Typography>
+                    </TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
                   {proposals
-                    ? proposals.map((proposal: ProposalType, index: number) => (
-                        <TableRow
-                          key={index}
-                          sx={{
-                            '&:last-child td, &:last-child th': {border: 0},
-                          }}>
-                          <TableCell align="right">
-                            {proposal.content.text}
-                          </TableCell>
-                          <TableCell align="right">{proposal.owner}</TableCell>
-                          <TableCell align="right">
-                            {proposal.timestamp}
-                          </TableCell>
-                          <TableCell align="right">
-                            {proposal.balance}
-                          </TableCell>
-                          <TableCell align="right">
-                            {/* {proposal.vote} */}
-                            null
-                          </TableCell>
-                          <TableCell align="right">{proposal.yes}</TableCell>
-                          <TableCell align="right">{proposal.no}</TableCell>
-                          <TableCell align="right">
-                            {proposal.abstain}
-                          </TableCell>
-                          <TableCell align="right">
-                            {proposal.successThreshold}
-                          </TableCell>
-                          <TableCell align="right">
-                            {proposal.failThreshold}
-                          </TableCell>
-                          <TableCell align="right">
-                            {proposal.isRelatedWithNft}
-                          </TableCell>
-                        </TableRow>
-                      ))
+                    ? proposals
+                        .slice(
+                          page * rowsPerPage,
+                          page * rowsPerPage + rowsPerPage,
+                        )
+                        .map((proposal: ProposalType, index: number) => (
+                          <TableRow
+                            key={index}
+                            sx={{
+                              '&:last-child td, &:last-child th': {border: 0},
+                            }}>
+                            <TableCell align="right">
+                              {proposal.content.text}
+                            </TableCell>
+                            <TableCell align="right">
+                              {proposal.owner}
+                            </TableCell>
+                            <TableCell align="right">
+                              {proposal.timestamp}
+                            </TableCell>
+                            <TableCell align="right">
+                              {proposal.balance}
+                            </TableCell>
+                            <TableCell align="right">
+                              {/* {proposal.vote} */}
+                              null
+                            </TableCell>
+                            <TableCell align="right">{proposal.yes}</TableCell>
+                            <TableCell align="right">{proposal.no}</TableCell>
+                            <TableCell align="right">
+                              {proposal.abstain}
+                            </TableCell>
+                            <TableCell align="right">
+                              {proposal.successThreshold}
+                            </TableCell>
+                            <TableCell align="right">
+                              {proposal.failThreshold}
+                            </TableCell>
+                            <TableCell align="right">
+                              {proposal.isRelatedWithNft}
+                            </TableCell>
+                          </TableRow>
+                        ))
                     : undefined}
                 </TableBody>
               </Table>
             </TableContainer>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, 100]}
+              component="div"
+              count={proposals!.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
           </Grid>
         </Grid>
       </div>
