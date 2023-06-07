cabf1f0f7bc55763ec40f89cb1510c78ed300774
Oğuzhan İpteş <53427115+oguzhaniptes@users.noreply.github.com>
2023-06-07 11:42:13 +0300

    ViewNFT: added collection title typography

--- src/pages/ViewNFT.tsx
+++ src/pages/ViewNFT.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Grid, Card, Typography, Box, CircularProgress, Theme, CardMedia, CardContent, Button } from "@mui/material";
+import { Grid, Card, Typography, Box, CircularProgress, Theme, CardMedia, CardContent, Button, Stack, Container, Divider } from "@mui/material";
 import { fetchNfts } from "../lib/api";
 import { useTonAddress } from "@tonconnect/ui-react";
 import { makeStyles } from "@mui/styles";
@@ -12,10 +12,25 @@ const useStyles = makeStyles((theme: Theme) => ({
     height: "80vh",
     widht: "100%",
     overflow: "auto",
-    padding: "1rem"
+    padding: "1rem",
+    alignContent: "start",
   },
   card: {
     margin: "1rem",
+  },
+  title: {
+    color: "#1689c5",
+    fontWeight: "bold",
+  },
+  divider:{
+    "&.MuiDivider-root": {
+      "&::before": {
+        borderTop: "thin solid #1689c5"
+      },
+      "&::after": {
+        borderTop: "thin solid #1689c5"
+      }
+    },
   },
 }))
 
@@ -70,69 +85,76 @@ const ViewNft = () => {
 
   return (
     <Grid container className={classes.container}>
-      {loading ? (
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "60vh",
-            width: "80vw",
-          }}
-        >
-          <CircularProgress />
-        </Box>
-      ) : (
-        <>
-          {collections.length === 0 && (
-            <Grid
-              item
-              md={12}
-              style={{
+      <Grid container direction={"column"}>
+        <Grid item textAlign={"center"} margin={2}>
+          <Divider className={classes.divider}>
+            <Typography className={classes.title} variant="h5">Collections</Typography>
+          </Divider>
+        </Grid>
+        <Grid container direction={"row"}>
+          {loading ? (
+            <Box
+              sx={{
+                display: "flex",
                 justifyContent: "center",
                 alignItems: "center",
-                display: "flex",
+                height: "60vh",
+                width: "80vw",
               }}
             >
-              <Typography style={{
-                color: "#1689c5",
-                fontSize: "30px",
-                fontWeight: "bold",
-              }}
-              >
-                There are no Collection's
-              </Typography>
-            </Grid>
-          )}
-          {
-            collections.map((item: any) => (
-              <Card className={classes.card} sx={{ width: 280, minHeight: 280, maxHeight: 300, }}
-              onClick={() => {
-                navigate("/view-nfts/" + item.collectionAddress);
-              }}>
-                <CardMedia image="https://www.webtekno.com/images/editor/default/0003/96/db58cdce487ce8c8c8d890916ef7cd5f6853c272.jpeg" sx={{ height: 120, }}></CardMedia>
-                <CardContent>
-                  <Grid marginBottom={2} item minHeight={"64px"}>
-                    <Typography variant="h6" gutterBottom component="div">
-                      {item.collectionName}
-                    </Typography>
-                  </Grid>
-                  <Grid container direction={"column"}>
-                    <Typography variant="body2" color="text.secondary">
-                      Collection address:
-                    </Typography>
-                    <Grid container direction={"row"} marginTop={1} justifyContent={"space-between"}>
-                      <Typography variant="body2" color="text.secondary">
-                        {item?.collectionAddress?.slice(0, 10) + "..." + item?.collectionAddress?.slice(-5)}
-                      </Typography>
-                      <Button startIcon={<ContentCopyIcon></ContentCopyIcon>} size="small" onClick={()=>{ navigator.clipboard.writeText(item?.collectionAddress);}}></Button>
-                    </Grid>
-                  </Grid>
-                </CardContent>
-              </Card>
-            ))
-          }
-          {/* {nfts.map((item: any) => (
+              <CircularProgress />
+            </Box>
+          ) : (
+            <>
+              {collections.length === 0 && (
+                <Grid
+                  item
+                  md={12}
+                  style={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                    display: "flex",
+                  }}
+                >
+                  <Typography style={{
+                    color: "#1689c5",
+                    fontSize: "30px",
+                    fontWeight: "bold",
+                  }}
+                  >
+                    There are no Collection's
+                  </Typography>
+                </Grid>
+              )}
+              {
+                collections.map((item: any) => (
+                  <Card className={classes.card} sx={{ width: 280, minHeight: 280, maxHeight: 300, }}
+                    onClick={() => {
+                      navigate("/view-nfts/" + item.collectionAddress);
+                    }}>
+                    <CardMedia image="https://www.webtekno.com/images/editor/default/0003/96/db58cdce487ce8c8c8d890916ef7cd5f6853c272.jpeg" sx={{ height: 120, }}></CardMedia>
+                    <CardContent>
+                      <Grid marginBottom={2} item minHeight={"64px"}>
+                        <Typography variant="h6" gutterBottom component="div">
+                          {item.collectionName}
+                        </Typography>
+                      </Grid>
+                      <Grid container direction={"column"}>
+                        <Typography variant="body2" color="text.secondary">
+                          Collection address:
+                        </Typography>
+                        <Grid container direction={"row"} marginTop={1} justifyContent={"space-between"}>
+                          <Typography variant="body2" color="text.secondary">
+                            {item?.collectionAddress?.slice(0, 10) + "..." + item?.collectionAddress?.slice(-5)}
+                          </Typography>
+                          <Button startIcon={<ContentCopyIcon></ContentCopyIcon>} size="small" onClick={() => { navigator.clipboard.writeText(item?.collectionAddress); }}></Button>
+                        </Grid>
+                      </Grid>
+                    </CardContent>
+                  </Card>
+                ))
+              }
+              {/* {nfts.map((item: any) => (
               <Grid item margin={1} md={3.75} justifyContent={"space-around"}>
                 <a style={{ textDecoration: "none" }} onClick={()=>console.log(item)}>
                   <NftCard
@@ -145,8 +167,10 @@ const ViewNft = () => {
                 </a>
               </Grid>
             ))} */}
-        </>
-      )}
+            </>
+          )}
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
